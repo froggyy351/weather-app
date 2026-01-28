631ae4c8e3c3d3c43fe31a1a1dd6aef629da184f
sakoichi <kouichi19980817@gmail.com>
2026-01-29 00:10:27 +0900

    feat: format weather data from API response

--- src/main/java/com/weather/App.java
+++ src/main/java/com/weather/App.java
@@ -2,6 +2,9 @@ package com.weather;
 
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.github.cdimascio.dotenv.Dotenv;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -20,6 +23,7 @@ public class App
         String apiKey = dotenv.get("WEATHER_API_KEY");
 
         String city = "Tokyo";
+        //metric = 摂氏
         String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
 
         OkHttpClient client = new OkHttpClient();
@@ -30,13 +34,29 @@ public class App
         try (Response response = client.newCall(request).execute()) {
             if(response.isSuccessful() && response.body() != null){
                 String result = response.body().string();
-                System.out.println("取得成功！");
-                System.out.println("--- 取得データ ---");
-                System.out.println(result);
+
+                //ここからjacksonによる解析
+                ObjectMapper mapper = new ObjectMapper();
+                //json文字列を木の枝(node)のような構造に変換する
+                JsonNode root = mapper.readTree(result);
+
+                //欲しい情報をpathを指定して掘り進む
+                String cityName = root.path("name").asText();
+                double temp = root.path("main").path("temp").asDouble();
+                int humidity = root.path("main").path("humidity").asInt();
+                String description = root.path("weather").get(0).path("description").asText();
+
+                //結果を表示
+                System.out.println("===" + cityName + "：現在の天気===");
+                System.out.println("天気　：　" + description);
+                System.out.println("気温　：　" + temp + "°C");
+                System.out.println("湿度　：　" + humidity + "%");
+                System.out.println("=============================");
             } else {
                 System.out.println("エラーが発生しました" + response.code());
             }
         } catch (IOException e) {
+            System.err.println("通信または解析中にエラーが発生しました。");
             e.printStackTrace();
         }
     }
